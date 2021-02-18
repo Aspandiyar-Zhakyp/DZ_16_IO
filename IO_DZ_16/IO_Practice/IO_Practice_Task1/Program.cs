7e83db299cc2b93a36db69e01b257672e084476b
Aspandiyar-Zhakyp <75847712+Aspandiyar-Zhakyp@users.noreply.github.com>
2021-02-18 12:45:05 +0600

    DZ16_IO_HW_and_Practice

---
+++ IO_DZ_16/IO_Practice/IO_Practice_Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace IO_Practice_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> stat = new Dictionary<char, int>();
+
+            using (FileStream stream = new FileStream(@"C:\Users\Esetb\source\repos\README.md", FileMode.OpenOrCreate))
+            {
+                byte[] bytes = new byte[stream.Length];
+
+                stream.Read(bytes, 0, bytes.Length);
+
+                string data = System.Text.Encoding.Unicode.GetString(bytes);
+                foreach (char symbol in data)
+                {
+                    if (stat.ContainsKey(symbol))
+                    {
+                        stat[symbol]++;
+                    }
+                    else
+                    {
+                        stat[symbol] = 0;
+                    }
+                }
+                Console.WriteLine(data);
+            }
+            foreach (KeyValuePair<char, int> item in stat)
+            {
+                Console.WriteLine(item.Key + "\t\t" + item.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IO_DZ_16/IO_Practice/IO_Practice_Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IO_Practice_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Esetb\source\repos\pwtask2.txt";
+            using (StreamWriter stream = new StreamWriter(path))
+            {
+                stream.WriteLine("Aspandiyar");
+                stream.WriteLine("Zhakyp");
+                stream.WriteLine("16");
+            }
+        }
+    }
+}
